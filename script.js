addf30ec38af83d9e11949c449dda0ff3bdacb2a
SelfwareEngineer <126501922+SelfwareEngineer@users.noreply.github.com>
2024-07-03 19:17:32 -0400

    Add operator function to call simple math functions

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+function operate(operand1, operator, operand2) {
+  switch (operator) {
+    case "+":
+      return add(operand1, operand2);
+      break;
+
+    case "-":
+      return subtract(operand1, operand2);
+      break;
+
+    case "*":
+      return multiply(operand1, operand2);
+      break;
+
+    case "/":
+      // TODO: - Snark for div/zero
+      return divide(operand1, operand2);
+      break;
+  }
+}
+
+let operand1;
+let operator;
+let operand2;
